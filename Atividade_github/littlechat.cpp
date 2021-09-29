31fa314a94d840572575be8fc984dfc6bae1a82a
Davi Chiune <91569678+Ch1un3@users.noreply.github.com>
2021-09-28 22:17:34 -0300

    Add files via upload

---
+++ Atividade_github/littlechat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string favorite_food, favorite_drink;
+    cout<<"What is your favorite food?"<<'\n';
+    cin>>favorite_food;
+    
+    cout<<"oh, that's really nice! And your favorite drink is?"<<'\n';
+    cin>>favorite_drink;
+
+    cout<<"So, your favorite food is "<<favorite_food<<" and your favorite drink is "<<favorite_drink<<", right?"<<'\n';
+    cout<<"You surely have a great taste!";
+
+    return 0;
+}
